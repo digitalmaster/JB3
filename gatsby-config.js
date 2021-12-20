8cf652a1f8249fd06581b8b4c4a6b241f170fac5
Jose Browne <josebrowne@gmail.com>
2021-12-20 12:40:45 -0400

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -184,7 +184,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GA_TRACKING_ID,
+        trackingId: process.env.GA_TRACKING_ID || 'dev',
       },
     },
     {
